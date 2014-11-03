fe7c05043dab4057fed2287479198a8d7c8ada78
stephanie pi <stephmpi@gmail.com>
2014-11-03 13:24:38 -0800

    project initialized

---
+++ poker_dice.rb
@@ -0,0 +1,22 @@
+class Poker_Dice
+
+  FACES = %w[ 9 T J Q K A ]
+
+  attr_reader :top_face
+
+  def initialize
+    roll
+  end
+
+  def roll
+    @top_face = FACES [ rand(0..5)]
+  end
+
+
+end
+
+d1 = Die.new.roll
+d2 = Die.new.roll
+d3 = Die.new.roll
+d4 = Die.new.roll
+d5 = Die.new.roll
---
+++ spec/poker_dice_spec.rb
@@ -0,0 +1,5 @@
+require './poker_dice.rb'
+
+describe Poker_Dice do
+
+end
