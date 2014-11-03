49f0ef4280e98d0041e221889f41a98ef05706b9
stephanie pi <stephmpi@gmail.com>
2014-11-03 14:30:02 -0800

    write test for unwritten roll_hand method

--- spec/poker_dice_spec.rb
+++ spec/poker_dice_spec.rb
@@ -2,8 +2,12 @@ require './poker_dice.rb'
 
 describe Poker_Dice do
 
-  it "returns an array" do
+  it "returns 9, T, J, Q, K, or A " do
     expect(Poker_Dice.new.roll).to match(/[9TJQKA]/)
+  end
+
+  it "returns an array with 5 items" do
+    expect(Poker_Dice.new.roll_hand.count).to eq 5
   end
 
 end
